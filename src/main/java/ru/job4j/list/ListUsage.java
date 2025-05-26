52e35b2c044efa56460203aafbb81dab1b729be4
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-05-26 23:57:21 +0300

    0.1. Интерфейс List [#4953 #429681]

---
+++ src/main/java/ru/job4j/list/ListUsage.java
@@ -0,0 +1,25 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListUsage {
+    public static void main(String[] args) {
+        List<String> result = new ArrayList<>();
+        result.add("one");
+        result.add("two");
+        result.add("three");
+        result.add(1,"four");
+        List<String> list =List.of("Five", "Six");
+        result.addAll(list);
+       /*for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }*/
+        result.set(2, "two and three");
+        Iterator<String> iterator = result.listIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
