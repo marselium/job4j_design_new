1132cfd5debb3977938f095d6a180e4924e6e0cf
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-07-02 23:48:28 +0300

    4. Потоки байтовых массивов (ByteArray Streams) [#504998 #452209]

--- src/main/java/ru/job4j/io/ByteArrayStream.java
+++ src/main/java/ru/job4j/io/ByteArrayStream.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ByteArrayStream {
     public static void main(String[] args) {
@@ -16,6 +18,12 @@ public class ByteArrayStream {
         ByteArrayInputStream stream1 = new ByteArrayInputStream(bytes1, 0, 4);
         while ((data = stream1.read()) != -1) {
             System.out.println((char) data);
+        }
+        try (FileOutputStream fileOutputStream = new FileOutputStream("data/message.txt")) {
+            byte[] bytes2 = "Message".getBytes();
+            fileOutputStream.write(bytes2);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
