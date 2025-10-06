9fecd5e79179a7eaa4d6b98004e7cfabf66d0f63
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-10-06 23:06:09 +0300

    7. Анализ доступности сервера [#859 #454125]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,7 +8,7 @@ public class Analysis {
              PrintWriter output = new PrintWriter(target)) {
                 String line;
                 boolean flag = true;
-                while ((line = input.readLine() ) != null) {
+                while ((line = input.readLine()) != null) {
                     String[] data = line.split(" ");
                     if (flag == ("400".equals(data[0]) || "500".equals(data[0]))) {
                         output.append(data[1]).append("; ").append(flag ? "" : System.lineSeparator());
