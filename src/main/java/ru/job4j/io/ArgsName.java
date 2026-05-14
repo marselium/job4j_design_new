3f8536db34326ac2c4269c939a14fce959ef8050
Marsel Yumagulov <marseliumagulov@yandex.ru>
2026-05-14 23:50:35 +0300

    13. Именованные аргументы [#295518 #454713]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -1,4 +1,66 @@
 package ru.job4j.io;
 
-public class ArgsName  {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String arg : args) {
+            String trimmed = arg.trim();
+            if (trimmed.isEmpty()) {
+                throw new IllegalArgumentException("Error: Argument is empty");
+            }
+            if (!trimmed.startsWith("-")) {
+                throw new IllegalArgumentException("Error: This argument '" + trimmed + "' does not start with a '-' character");
+            }
+            if (!trimmed.contains("=")) {
+                throw new IllegalArgumentException("Error: This argument '" + trimmed + "' does not contain an equal sign");
+            }
+            String[] maps = trimmed.split("=", 2);
+            String key = maps[0].trim();
+            if (!key.startsWith("-")){
+                throw new IllegalArgumentException("Key must start with '-'");
+            }
+            key = key.substring(1);
+            String val = maps[1].trim();
+            if (key.isEmpty()) {
+                throw new IllegalArgumentException("Error: This argument '" + trimmed + "' does not contain a key");
+            }
+            if (val.isEmpty()) {
+                throw new IllegalArgumentException("Error: This argument '" + trimmed + "' does not contain a value");
+            }
+            if (values.containsKey(key)) {
+                throw new IllegalArgumentException("Duplicate key: " + key);
+            }
+
+            values.put(key, val);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
 }
