1c93f5e0b67465140af26303b31624d10923ea08
Marsel Yumagulov <marseliumagulov@yandex.ru>
2026-05-14 23:09:40 +0300

    12. Валидация параметров запуска [#246865 #454705]

--- src/main/java/ru/job4j/io/Dir1.java
+++ src/main/java/ru/job4j/io/Dir1.java
@@ -1,21 +1,22 @@
 package ru.job4j.io;
 
 import java.io.File;
-import java.text.Format;
 
 public class Dir1 {
     public static void main(String[] args) {
-        File file = new File("c:\\\\projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Директория не существует: %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Это не директория: %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("Размер директории: %s", file.getTotalSpace()));
-        String sep = System.lineSeparator();
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.printf("File name is : %s ,  size is: %d " + sep, subfile.getName() , subfile.length());
+            System.out.println(subfile.getName());
         }
     }
 }
